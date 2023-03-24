b55fe53b21246fcd453c476a9313ba47fd586ef9
harr-sudo <67125140+harr-sudo@users.noreply.github.com>
2023-03-24 16:24:56 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -127,7 +127,7 @@ cd /opt/tools && sudo git clone https://github.com/nabla-c0d3/sslyze.gitsudo && 
 cd /opt/tools && sudo git clone https://github.com/t3l3machus/psudohash  
 cd /opt/tools/psudohash && chmod +x psudohash.py 
 cd /opt/tools/ && sudo pip install shell-gpt
-cd /opt/tools && sudo git clone https://github.com/IvanGlinkin/Domain_checker.git && chmod +x domain_checker.sh
+cd /opt/tools && sudo git clone https://github.com/IvanGlinkin/Domain_checker.git && sudo chmod +x domain_checker.sh
 
 cd /opt/tools && sudo mkdir AD
 
